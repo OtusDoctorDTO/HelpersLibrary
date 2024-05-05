87ce9b5c66edd1f80414a5458c2ef4dc9a3b794d
KozyrevaSvetlana <lanako0507@gmail.com>
2024-05-05 22:13:10 +0300

    LoginDTO

---
+++ HelpersDTO/Authentication/DTO/Models/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpersDTO.Authentication.DTO.Models
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
