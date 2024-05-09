0054bed328530c3d3008414254735da366c8c493
KozyrevaSvetlana <lanako0507@gmail.com>
2024-05-09 22:10:01 +0300

    Short Data

---
+++ HelpersDTO/AppointmentDto/DTO/ShortAppointnmentDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace HelpersDTO.AppointmentDto.DTO
+{
+    public class ShortAppointnmentDTO
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Время начала записи
+        /// </summary>
+        public DateTime StartDate { get; set; }
+        /// <summary>
+        /// Идентификатор врача
+        /// </summary>
+        public Guid DoctorId { get; set; }
+    }
+}
---
+++ HelpersDTO/Doctor/DTO/Models/ShortDoctorDTO.cs
@@ -0,0 +1,22 @@
+﻿using HelpersDTO.Base.Models;
+
+namespace HelpersDTO.Doctor.DTO.Models
+{
+    /// <summary>
+    /// Доктор
+    /// </summary>
+    public class ShortDoctorDTO
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// ФИО
+        /// </summary>
+        public BaseUserDTO User { get; set; }
+
+        /// <summary>
+        /// Специальность
+        /// </summary>
+        public string Specialty { get; set; }
+    }
+}
