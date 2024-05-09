c14f858d6fe0e8ebb97875a91c706c9aba726515
KozyrevaSvetlana <lanako0507@gmail.com>
2024-05-09 19:13:34 +0300

    ctor RegistrationResponse

--- HelpersDTO/Authentication/RegistrationResponce.cs
+++ HelpersDTO/Authentication/RegistrationResponce.cs
@@ -5,5 +5,9 @@
         public bool Flag { get; }
         public List<string> Messages { get; set; } = new();
         public string? Token { get; set; } = string.Empty;
+        public RegistrationResponse(bool flag)
+        {
+            Flag = flag;
+        }
     }
 }
