6bee3f38bf3781ce742e513b054c383e0f04e9ac
olya_nick <olya17nikolbas@gmail.com>
2024-05-26 13:17:43 +0300

    Добавлен model DTO для пациента

---
+++ HelpersDTO/Patient/DTO/PatientDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpersDTO.Patient.DTO
+{
+    public class PatientDTO
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string? MiddleName { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Телефон
+        /// </summary>
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public Guid? UserId { get; set; }
+
+        /// <summary>
+        /// Статус
+        /// </summary>
+        public string? Status { get; set; } = default;
+    }
+}
