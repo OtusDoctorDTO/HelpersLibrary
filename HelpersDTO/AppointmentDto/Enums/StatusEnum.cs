f86b8c66b5c81dcbe2a43ed7779c445eb430b888
KozyrevaSvetlana <lanako0507@gmail.com>
2024-06-01 09:56:46 +0300

    StatusEnum добавлены новые статусы

--- HelpersDTO/AppointmentDto/Enums/StatusEnum.cs
+++ HelpersDTO/AppointmentDto/Enums/StatusEnum.cs
@@ -1,14 +1,25 @@
-﻿namespace HelpersDTO.AppointmentDto.Enums
+﻿using System.ComponentModel;
+
+namespace HelpersDTO.AppointmentDto.Enums
 {
     /// <summary>
     /// Статус записи
     /// </summary>
     public enum StatusEnum
     {
+        [Description("")]
         None = 0,
+        [Description("Свободно")]
         Free,
-        Success,
+        [Description("Забронировано пользователем")]
+        BookedByUser,
+        [Description("В ожидании приема")]
         Waiting,
+        [Description("Прием начат")]
+        InProccess,
+        [Description("Завершено")]
+        Success,
+        [Description("Отменено")]
         Сanceled
     }
 }
