8b8cd78cb30a631ac9f5744c6eadfb0f382c05c2
KozyrevaSvetlana <lanako0507@gmail.com>
2024-06-02 20:50:54 +0300

    новый статус для записи

--- HelpersDTO/AppointmentDto/Enums/StatusEnum.cs
+++ HelpersDTO/AppointmentDto/Enums/StatusEnum.cs
@@ -9,16 +9,39 @@ namespace HelpersDTO.AppointmentDto.Enums
     {
         [Description("")]
         None = 0,
+        /// <summary>
+        /// Свободная запись
+        /// </summary>
         [Description("Свободно")]
         Free,
+        /// <summary>
+        /// Пользователь записался самостоятельно на сайте
+        /// </summary>
         [Description("Забронировано пользователем")]
         BookedByUser,
+        /// <summary>
+        /// Когда в колл центре подтвердили запись
+        /// </summary>
+        [Description("Подтверждено")]
+        Confirmed,
+        /// <summary>
+        /// В ожидании приема в клинике
+        /// </summary>
         [Description("В ожидании приема")]
         Waiting,
+        /// <summary>
+        /// Доктор начал прием
+        /// </summary>
         [Description("Прием начат")]
         InProccess,
+        /// <summary>
+        /// Запись завершена
+        /// </summary>
         [Description("Завершено")]
         Success,
+        /// <summary>
+        /// Запись отменена
+        /// </summary>
         [Description("Отменено")]
         Сanceled
     }
